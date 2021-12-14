81a59445218b7d29559c96e2ffd62593f02f61e3
Pedro Henrique <phfs28@gmail.com>
2021-12-14 09:12:56 -0300

    Added Menu and Close icons

---
+++ assets/images/icons/Close.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg class="app-icon" width="60" height="60" viewBox="0 0 60 60" fill="none">
+    <path d="M52.5007 0L29.9973 22.498L7.49933 0L0 7.49934L22.498 29.9973L0 52.4954L7.49933 59.9947L29.9973 37.4967L52.5007 59.9947L60 52.4954L37.502 29.9973L60 7.49934L52.5007 0Z" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconClose',
+};
+</script>
---
+++ assets/images/icons/Menu.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg class="app-icon" width="60" height="40" viewBox="0 0 60 40" fill="none">
+    <path d="M0 40H60V33.3333H0V40ZM0 23.3333H60V16.6667H0V23.3333ZM0 0V6.66667H60V0H0Z" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconMenu',
+};
+</script>
