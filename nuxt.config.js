0c33809c797ec688ebc228f7379c28ba2f8c1dd2
Pedro Henrique <phfs28@gmail.com>
2021-12-14 08:17:59 -0300

    Added Nuxt network server configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  server: {
+    host: '0.0.0.0',
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'hogwarts-school',
