4b9bd60067b72592d8fc2a6049c2a935b067f4a0
Pedro <phfs28@gmail.com>
2021-12-16 20:17:32 -0300

    Added call-to-action's icon

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -1,20 +1,33 @@
 <template>
   <a href="#" class="call-to-action">
     <slot />
+    <IconArrow class="call-to-action__icon" />
   </a>
 </template>
 
 <script>
+import IconArrow from '@/assets/images/icons/Arrow.vue';
+
 export default {
   name: 'CallToAction',
+  components: {
+    IconArrow,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .call-to-action {
+  display: inline-block;
+  height: 56px;
   color: $white;
   background-color: $red-1;
   padding: 16px 36px;
   text-transform: uppercase;
+  &__icon {
+    fill: $white;
+    height: 12px;
+    padding-left: 16px;
+  }
 }
 </style>
