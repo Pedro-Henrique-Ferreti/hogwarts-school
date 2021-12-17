3ff3355b6815c28e0c5024a517b3b7c3b26420c0
Pedro Henrique <phfs28@gmail.com>
2021-12-17 18:00:59 -0300

    Created CardAnimals

--- components/SectionAboutCardAnimals.vue
+++ components/SectionAboutCardAnimals.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="card-animals"></div>
+  <div class="card-animals">
+    <div class="card-animals__background-image" />
+    <div class="card-animals__content">
+      <div class="card-animals__title-wrapper">
+        <h3 class="card-animals__title">Hogwarts</h3>
+        <h4 class="card-animals__subtitle">Animals & Beasts</h4>
+      </div>
+      <CallToAction class="card-animals__button">Learn More</CallToAction>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,8 +19,37 @@ export default {
 
 <style lang="scss" scoped>
 .card-animals {
-  background-color: $golden-1;
-  width: 100%;
-  height: 300px;
+  @include card-height;
+  @include scale-card-background-image;
+  position: relative;
+  &__background-image {
+    height: inherit;
+    background-image: url('@/assets/images/animal-owl.png');
+    background-size: cover;
+    background-position: center;
+  }
+  &__content {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: $mobile-padding;
+    @media (min-width: $large-tablet-portrait-up) {
+      margin: $desktop-padding;
+    }
+  }
+  &__title {
+    color: $red-1;
+    font-size: 24px;
+    text-transform: uppercase;
+  }
+  &__subtitle {
+    color: $golden-1;
+  }
+  &__button {
+    position: absolute;
+    bottom: 0;
+  }
 }
 </style>
