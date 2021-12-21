9a2e24a1d0a8aebee0120231c834c54c9c613480
Pedro Henrique <phfs28@gmail.com>
2021-12-21 10:54:58 -0300

    Created the card House Heads

--- components/SectionHousesCardHouseHeads.vue
+++ components/SectionHousesCardHouseHeads.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="card-house-heads"></div>
+  <div class="card-house-heads">
+    <div class="card-house-heads__background-image" />
+    <CallToAction class="card-house-heads__button">Heads of The Houses</CallToAction>
+  </div>
 </template>
 
 <script>
@@ -10,8 +13,25 @@ export default {
 
 <style lang="scss" scoped>
 .card-house-heads {
-  background-color: $blue-1;
-  width: 100%;
-  height: 300px;
+  @include card-height;
+  @include scale-card-background-image;
+  position: relative;
+  &__background-image {
+    width: 100%;
+    height: inherit;
+    background-image: url('@/assets/images/house-inside.png');
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+  &__button {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    padding-top: 8px;
+    padding-bottom: 8px;
+    height: 44px;
+  }
 }
 </style>
