e0658dbe6ad427f2535c0735e3d62af7ed6883a7
Pedro <phfs28@gmail.com>
2021-12-27 20:46:31 -0300

    Added black cat image

--- components/SectionAboutCardAnimals.vue
+++ components/SectionAboutCardAnimals.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="card-animals">
-    <div class="card-animals__background-image" />
+    <div class="card-animals__background-image"
+      :key="key"
+      :class="cardClasses"
+    />
     <div class="card-animals__content">
       <div class="card-animals__title-wrapper">
         <h3 class="card-animals__title">Hogwarts</h3>
@@ -14,6 +17,24 @@
 <script>
 export default {
   name: 'SectionAboutCardAnimals',
+  data() {
+    return {
+      key: 0,
+    };
+  },
+  computed: {
+    cardClasses() {
+      return {
+        'card-animals__background-image--owl': this.key % 2 !== 0,
+        'card-animals__background-image--cat': this.key % 2 === 0,
+      };
+    },
+  },
+  mounted() {
+    setInterval(() =>{
+      this.key++;
+    }, 7000);
+  },
 };
 </script>
 
@@ -21,12 +42,20 @@ export default {
 .card-animals {
   @include card-height;
   @include scale-card-background-image;
+  background-color: $gray-2;
   position: relative;
   &__background-image {
     height: inherit;
-    background-image: url('@/assets/images/animal-owl.webp');
     background-size: cover;
     background-position: center;
+    &--owl {
+      background-image: url('@/assets/images/animal-owl.webp');
+      animation: card-background $animation-duration__slow ease-in-out;
+    }
+    &--cat {
+      background-image: url('@/assets/images/black-cat.jpg');
+      animation: card-background $animation-duration__slow ease-in-out;
+    }
   }
   &__content {
     position: absolute;
@@ -50,6 +79,11 @@ export default {
   &__button {
     position: absolute;
     bottom: 0;
+  }
+}
+@keyframes card-background {
+  from {
+    opacity: 0.25;
   }
 }
 </style>
